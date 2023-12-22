d73bd07f6bfa4c017b9840c02eeccc66be4825d7
Boyuan Yu <yu.boyu@northeastern.edu>
2023-12-21 18:29:15 -0800

    added icons

--- src/components/Framer.js
+++ src/components/Framer.js
@@ -1,14 +1,28 @@
 import { motion } from "framer-motion"
 import { Container, Row, Col, Tabs, Tab, Nav } from "react-bootstrap";
 
+const Skill = ({ name, x, y, img }) => {
+  return (
+    <motion.div className='flex items-center justify-center rounded-full absolute p-8'
+      whileHover={{ scale: 1.1 }}
+      initial={{ x: 0, y: 0 }}
+      whileInView={{ x: x, y: y }}
+      transition={{ duration: 1.5 }}
+    >
+      {name}
+    </motion.div>
+  )
+}
 
 export const Framer = () => {
+
   return (
     <section className="framer" id="framer">
       <Container>
         <h2>My Skills</h2>
         <div className="w-full h-full relative flex items-center justify-center rounded-full bg-circularLight">
-
+          <Skill name="React" x={'-5vw'} y={'-10vw'} />
+          <Skill name="React Native" x={'0'} y={'0'} />
         </div>
       </Container>
 
