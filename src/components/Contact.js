4db3e2c8610e886c004e2bf112650be4bfdf43ab
Boyuan Yu <yu.boyu@northeastern.edu>
2024-01-02 18:31:07 -0800

    fix typo

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -63,7 +63,7 @@ export const Contact = () => {
                     <input type="text" value={formDetails.firstName} placeholder="First Name" onChange={(e) => onFormUpdate('firstName', e.target.value)} />
                   </Col>
                   <Col xs={12} sm={6} className="px-1">
-                    <input type="text" value={formDetails.lasttName} placeholder="Last Name" onChange={(e) => onFormUpdate('lastName', e.target.value)} />
+                    <input type="text" value={formDetails.lastName} placeholder="Last Name" onChange={(e) => onFormUpdate('lastName', e.target.value)} />
                   </Col>
                   <Col xs={12} sm={12} className="px-1">
                     <input type="email" value={formDetails.email} placeholder="Email Address" onChange={(e) => onFormUpdate('email', e.target.value)} />
