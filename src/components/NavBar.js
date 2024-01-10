ce7159e450846f8bb640b918dbc611e640a5be68
Boyuan Yu <yu.boyu@northeastern.edu>
2024-01-10 13:44:36 -0800

    fix navbar phone view

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { Navbar, Nav, Container } from "react-bootstrap";
+import { Navbar, Nav, Container, Row } from "react-bootstrap";
 import logo from '../assets/img/logo-brian-removebg.png';
 import githubIcon from '../assets/img/github-mark.svg';
 import linkedInIcon from '../assets/img/iconmonstr-linkedin-4.svg';
@@ -37,7 +37,9 @@ export const NavBar = () => {
           <Navbar.Toggle aria-controls="basic-navbar-nav">
             <span className="navbar-toggler-icon"></span>
           </Navbar.Toggle>
-
+          <Navbar.Brand href="/" className="ms-auto">
+            <img src={logo} alt="Logo" />
+          </Navbar.Brand>
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav>
               <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
@@ -45,9 +47,6 @@ export const NavBar = () => {
               <Nav.Link href="#framer" className={activeLink === 'framer' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('framer')}>Skills</Nav.Link>
               <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
             </Nav>
-            <Navbar.Brand href="/" className="mx-auto">
-              <img src={logo} alt="Logo" />
-            </Navbar.Brand>
             <span className="navbar-text ms-auto">
               <div className="social-icon">
                 <a href="https://github.com/Boyuan-Yu"><img src={githubIcon} alt="" /></a>
@@ -60,6 +59,9 @@ export const NavBar = () => {
               </a>
             </span>
           </Navbar.Collapse>
+
+
+
         </Container>
       </Navbar>
     </Router>
